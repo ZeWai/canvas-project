0072a6020eb1b534b3b1ca81f9b8119741a7123b
Zewai <choi.wai1130@gmail.com>
2021-11-09 11:52:58 +0800

    9Nov

--- javascript/drawing-circle.js
+++ javascript/drawing-circle.js
@@ -1,40 +1,44 @@
 class DrawingCircle extends PaintFunction {
-     constructor(contextReal, contextDraft) {
-         super();
-         this.contextReal = contextReal;
-         this.contextDraft = contextDraft;
-     }
+    constructor(contextReal, contextDraft) {
+        super();
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;
+    }
 
-     onMouseDown(coord, e) {
-         this.contextReal.fillStyle = color;
-         this.origX = coord[0];
-         this.origY = coord[1];
+    onMouseDown(coord, e) {
+        this.contextReal.fillStyle = color;
+        this.origX = coord[0];
+        this.origY = coord[1];
 
-     }
+    }
 
-     onDragging(coord, e) {
-         this.contextDraft.fillStyle = color;
-         var NewX = Math.abs(this.origX - coord[0]);
-         var NewY = Math.abs(this.origY - coord[1]);
-         this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-         this.contextDraft.beginPath();
-         this.contextDraft.ellipse(this.origX, this.origY,NewX,NewY, Math.PI / 4,0, Math.PI*2);
-         this.contextDraft.fill();
+    onDragging(coord, e) {
+        this.contextDraft.fillStyle = color;
+        var NewX = Math.abs(this.origX - coord[0])/2;
+        var NewY = Math.abs(this.origY - coord[1])/2;
+        var centerX= (this.origX-(this.origX - coord[0])/2);
+        var centerY= (this.origY-(this.origY - coord[1])/2);
+        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+        this.contextDraft.beginPath();
+        this.contextDraft.ellipse(centerX, centerY,NewX,NewY, 0,0, Math.PI*2);
+        this.contextDraft.fill();
 
-     }
+    }
 
-     onMouseMove(){}
+    onMouseMove(){}
 
-     onMouseUp(coord) {
-         var NewX = Math.abs(this.origX - coord[0]);
-         var NewY = Math.abs(this.origY - coord[1]);
-         this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-         this.contextReal.beginPath();
-         this.contextReal.ellipse(this.origX, this.origY,NewX, NewY, Math.PI / 4,0, Math.PI*2);
-         this.contextReal.fill();
-     }
+    onMouseUp(coord) {
+        var NewX = Math.abs(this.origX - coord[0])/2;
+        var NewY = Math.abs(this.origY - coord[1])/2;
+        var centerX= (this.origX-(this.origX - coord[0])/2);
+        var centerY= (this.origY-(this.origY - coord[1])/2);
+        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+        this.contextReal.beginPath();
+        this.contextReal.ellipse(centerX, centerY,NewX, NewY,0,0, Math.PI*2);
+        this.contextReal.fill();
+    }
 
-     onMouseLeave() {}
-     onMouseEnter() {}   
+    onMouseLeave() {}
+    onMouseEnter() {}   
 
  }
--- javascript/drawing-strokecircle.js
+++ javascript/drawing-strokecircle.js
@@ -7,20 +7,22 @@ class DrawingStrokeCircle extends PaintFunction {
 
      onMouseDown(coord, e) {
          this.contextReal.strokeStyle = color;
-    this.contextReal.lineWidth = linewidth;
+         this.contextReal.lineWidth = linewidth;
          this.origX = coord[0];
          this.origY = coord[1];
-
+        
      }
 
      onDragging(coord, e) {
          this.contextDraft.strokeStyle = color;
-    this.contextDraft.lineWidth = linewidth;
-         var NewX = Math.abs(this.origX - coord[0]);
-         var NewY = Math.abs(this.origY - coord[1]);
+         this.contextDraft.lineWidth = linewidth;
+         var NewX = Math.abs(this.origX - coord[0])/2;
+         var NewY = Math.abs(this.origY - coord[1])/2;
+         var centerX= (this.origX-(this.origX - coord[0])/2);
+         var centerY= (this.origY-(this.origY - coord[1])/2);
          this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
          this.contextDraft.beginPath();
-         this.contextDraft.ellipse(this.origX, this.origY,NewX,NewY, Math.PI / 4,0, Math.PI*2);
+         this.contextDraft.ellipse(centerX, centerY,NewX,NewY, 0,0, Math.PI*2);
          this.contextDraft.stroke();
 
      }
@@ -28,11 +30,13 @@ class DrawingStrokeCircle extends PaintFunction {
      onMouseMove(){}
 
      onMouseUp(coord) {
-         var NewX = Math.abs(this.origX - coord[0]);
-         var NewY = Math.abs(this.origY - coord[1]);
+         var NewX = Math.abs(this.origX - coord[0])/2;
+         var NewY = Math.abs(this.origY - coord[1])/2;
+         var centerX= (this.origX-(this.origX - coord[0])/2);
+         var centerY= (this.origY-(this.origY - coord[1])/2);
          this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
          this.contextReal.beginPath();
-         this.contextReal.ellipse(this.origX, this.origY,NewX, NewY, Math.PI / 4,0, Math.PI*2);
+         this.contextReal.ellipse(centerX, centerY,NewX, NewY, 0,0, Math.PI*2);
          this.contextReal.stroke();
      }
 
