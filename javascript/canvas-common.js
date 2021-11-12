bb127fb5e04c2238ce6ddf3c38aa0583fbe0a632
Ivan Cheng <ivanchengzhongyan@gmail.com>
2021-11-12 16:10:46 +0800

    text fonts and sizing

--- javascript/canvas-common.js
+++ javascript/canvas-common.js
@@ -9,6 +9,7 @@ let canvasDraft = document.getElementById("canvas-draft");
 let contextDraft = canvasDraft.getContext("2d");
 let currentFunction;
 let dragging = false;
+let keyListeners = {shift: false, escape: false}
 
 $("#canvas-draft").mousedown(function (e) {
   let mouseX = e.offsetX;
--- javascript/text.js
+++ javascript/text.js
@@ -1,3 +1,4 @@
+let existingTextBox = false;  
 
 
 
@@ -6,32 +7,70 @@ class Text extends PaintFunction {
         super();
         this.contextReal = contextReal;
         this.contextDraft = contextDraft;
+        this.origX = null;
+        this.origY = null;
     }
 
     
     onMouseDown(coord, event) {
-        this.origX = coord[0];
-        this.origY = coord[1];
-        var input = document.createElement('input');
-        input.type ='text';
-        input.style.position = 'fixed';
-        input.style.left = (this.origX + 170) + 'px';
-        input.style.top = (this.origY + 80) + 'px';
-        
-        input.id = 'textBox'
-        document.body.appendChild(input);
+        if (existingTextBox == false){
+            this.origX = coord[0];
+            this.origY = coord[1];
+            var input = document.createElement('input');
 
-        input.onkeydown = function handleEnter(input) {
-            if (input.key == 'Enter') {
-                this.typedText= document.getElementById("textBox").value;
-                contextReal.fillText(this.typedText,event.clientX - 170,event.clientY - 80);
-                document.body.removeChild(this);
-                
-                beforeDraw();
-            }
-      };
+            input.setAttribute('class', 'inputBox');
+            
+            input.type ='text';
+            input.style.position = 'fixed';
+            input.placeholder = "Enter text here!";
+            input.style.left = event.clientX + 'px';
+            input.style.top = event.clientY + 'px';
+            this.contextReal.font = `${textSettings.textSize}px ${textSettings.textFont}`;
+            input.style.color = color;
+            input.style.font = `${textSettings.textSize}px ${textSettings.textFont}`;
+            input.style.border = '2px dotted black';
+            input.id = 'textBox'
+            document.body.appendChild(input);
+            existingTextBox = true;
+  
+        }
 
       
+            let ctxReal = this.contextReal;
+
+         
+
+    
+            input.onkeydown = function handleEnter(input) {
+                if (input.key == 'Enter') {
+                    this.typedText= document.getElementById("textBox").value;
+                    ctxReal.textBaseline = "middle";
+                    contextReal.fillStyle = color;
+
+                    ctxReal.fillText(this.typedText,event.offsetX,event.offsetY);
+                    
+                    
+                    document.body.removeChild(this);
+                    existingTextBox = false;
+
+                    
+                    beforeDraw();
+                    existingTextBox = false;
+                }
+
+              
+            if(input.key === 'Escape') {
+                $('.inputBox').remove();
+                existingTextBox = false;
+            }
+             };
+
+             
+
+             
+            
+        
+     
     
     
 
@@ -41,9 +80,7 @@ class Text extends PaintFunction {
     onDragging(){}
     onMouseMove(){}
     onMouseUp(){}
-    onMouseLeave(coord){
-
-    }
+   
     onMouseEnter(){}
 
 
