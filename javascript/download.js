0f81876f4683b9e9e0cc369f8e0ccb144c0f59a0
Ivan Cheng <ivanchengzhongyan@gmail.com>
2021-11-15 11:23:56 +0800

    Final

--- javascript/download.js
+++ javascript/download.js
@@ -3,16 +3,20 @@
 const reader = new FileReader();
 const img = new Image();
 
+
+
 const uploadImage = (e) => {
     reader.onload = () => {
         img.onload = () => {
-            canvasReal.width = img.width;
-            canvasReal.height = img.height;
-            contextReal.drawImage(img, 200, 200);
+            
+            contextReal.drawImage(img, 100, 100, canvasReal.width/2.5, canvasReal.height/2.5);
+            autosave();
+
         };
         img.src = reader.result;
     };
     reader.readAsDataURL(e.target.files[0]);
+
 };
 const imageLoader = document.getElementById('uploader')
 imageLoader.addEventListener('change', uploadImage);
